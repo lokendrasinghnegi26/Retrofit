5df8b3042ef7b063613c84e19e93c00957f207e5
LokendraSinghNegi <61650132+LokendraSinghNegi@users.noreply.github.com>
2021-05-14 20:43:22 +0530

    updated

--- app/src/main/java/com/lokendrasingh/retrofit/CustomAdaptor.java
+++ app/src/main/java/com/lokendrasingh/retrofit/CustomAdaptor.java
@@ -1,11 +1,18 @@
 package com.lokendrasingh.retrofit;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.widget.TextViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,22 +30,36 @@ public class CustomAdaptor extends RecyclerView.Adapter<CustomAdaptor.CustomHold
     @NonNull
     @Override
     public CustomHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater= LayoutInflater.from(parent.getContext());
+        View view= layoutInflater.inflate(R.layout.adaptor,parent,false);
+        return new CustomHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CustomHolder holder, int position) {
+        holder.textView.setText(datalist.get(position).getTitle());
+
+        Picasso.Builder builder = new Picasso.Builder(context);
+        builder.downloader(new OkHttp3Downloader(context));
+        builder.build().load(datalist.get(position).getThumbnailUrl())
+                .placeholder((R.drawable.ic_launcher_background))
+                .error(R.drawable.ic_launcher_background)
+                .into(holder.img);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return datalist.size();
     }
 
     public class CustomHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+        private ImageView img;
         public CustomHolder(@NonNull View itemView) {
             super(itemView);
+            textView= itemView.findViewById(R.id.textView);
+            img= itemView.findViewById(R.id.imageView);
         }
     }
 }
--- app/src/main/java/com/lokendrasingh/retrofit/RetroPhoto.java
+++ app/src/main/java/com/lokendrasingh/retrofit/RetroPhoto.java
@@ -14,6 +14,46 @@ public class RetroPhoto {
     @SerializedName("thumbnailUrl")
     private String thumbnailUrl;
 
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
     private RetroPhoto(int albumId, int id, String title, String url, String thumbnailUrl) {
         this.albumId = albumId;
         this.id = id;
